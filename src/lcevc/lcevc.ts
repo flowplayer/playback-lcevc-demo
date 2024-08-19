14a3cff252f3fcc2a622eb5e9fd3651d952bba24
Benjamin Clos <benjamin.clos@gmail.com>
2024-08-19 15:19:22 -0600

    fix: hack hls@1.0.2 into an esm context;

--- src/lcevc/lcevc.ts
+++ src/lcevc/lcevc.ts
@@ -8,10 +8,10 @@ export class LCEVC {
   canvas: HTMLCanvasElement = document.createElement("canvas")
   hls : typeof window.Hls
 
-  constructor (readonly player: Player, readonly config : any) {
+  constructor (readonly Hls: any, readonly player: Player, readonly config : any) {
     this.config = {...config, logLevel: 5}
     this.canvas.className = "fp-lcevc-output"
-    this.hls = new window.Hls({
+    this.hls = new Hls({
       //enableWorker: false
     })
     this.player.root.insertBefore(this.canvas, this.player.nextSibling)
@@ -26,14 +26,14 @@ export class LCEVC {
   }
 
   private attachEventHandlers () {
-    this.hls.on(window.Hls.Events.MEDIA_ATTACHING, (_ : any, data : any)=> {
+    this.hls.on(this.Hls.Events.MEDIA_ATTACHING, (_ : any, data : any)=> {
       console.log("creating LCEVC instance")
       this.decoder = new window.LCEVCdec.LCEVCdec(data.media, this.canvas, this.config)
       this.player.setState("is-lcevc", true)
       this.player.setState("is-starting", true)
     })
 
-    this.hls.on(window.Hls.Events.MEDIA_DETACHING, ()=> {
+    this.hls.on(this.Hls.Events.MEDIA_DETACHING, ()=> {
       if (this.decoder) {
         console.log("destroying LCEVC instance")
         this.decoder.close()
@@ -41,14 +41,14 @@ export class LCEVC {
       }
     })
 
-    this.hls.on(window.Hls.Events.MANIFEST_LOADING, () => {
+    this.hls.on(this.Hls.Events.MANIFEST_LOADING, () => {
         if (this.decoder) {
           console.log("manifest loading - calling resetDecoder()")
           this.decoder.resetDecoder()
         }
       })
 
-      this.hls.on(window.Hls.Events.BUFFER_APPENDING, (_ : any,data : any) => {
+      this.hls.on(this.Hls.Events.BUFFER_APPENDING, (_ : any,data : any) => {
         if (!this.decoder) return console.log("buffer available but lcevcDil not initialised")
         switch (data.type) {
           case "audiovideo":
@@ -61,25 +61,25 @@ export class LCEVC {
         }
       })
 
-      this.hls.on(window.Hls.Events.BUFFER_FLUSHING, (_ : any,data : any) => {
+      this.hls.on(this.Hls.Events.BUFFER_FLUSHING, (_ : any,data : any) => {
         if (!this.decoder) return
         console.log("buffer flushing - calling flushBluffer()")
         this.decoder.flushBuffer(data.startOffset, data.endOffset)
       })
 
-      this.hls.on(window.Hls.Events.LEVEL_PTS_UPDATED, (_ : any,data : any) => {
+      this.hls.on(this.Hls.Events.LEVEL_PTS_UPDATED, (_ : any,data : any) => {
         if (!this.decoder) return
         console.log("pts updated, calling newPts()")
         this.decoder.newPts(data.start, data.end, data.type, data.level.id, data.drift)
       })
 
-      this.hls.on(window.Hls.Events.LEVEL_SWITCHING, (_ : any,data : any) => {
+      this.hls.on(this.Hls.Events.LEVEL_SWITCHING, (_ : any,data : any) => {
         if (!this.decoder) return
         console.log("level switching, calling resetBuffer() and setLevelSwitching()")
         this.decoder.setLevelSwitching(data.level)
       })
 
-      this.hls.on(window.Hls.Events.LEVEL_SWITCHED, (_ : any, data : any) => {
+      this.hls.on(this.Hls.Events.LEVEL_SWITCHED, (_ : any, data : any) => {
         if (!this.decoder) return
         console.log("level switched, calling setCurrentLevel())")
         this.decoder.setCurrentLevel(data.level)
--- src/use-flowplayer.ts
+++ src/use-flowplayer.ts
@@ -2,8 +2,8 @@ import flowplayer from "@flowplayer/player"
 import HlsPlugin from "@flowplayer/player/plugins/hls"
 import QSELPlugin from "@flowplayer/player/plugins/qsel"
 
-flowplayer(HlsPlugin, QSELPlugin)
+const flowplayerWithPlugins = flowplayer(HlsPlugin, QSELPlugin)
 
 export function useFlowplayer () {
-    return flowplayer
+    return flowplayerWithPlugins
 }
